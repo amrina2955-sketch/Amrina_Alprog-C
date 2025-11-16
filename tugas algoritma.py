e95c2f4ff2542efc5d15ae749250af41aa08319a
Amrina_Alprog-C <amrina2955@gmail.com>
2025-11-16 23:20:42 +0800

    Add files via upload

---
+++ tugas algoritma.py
@@ -0,0 +1,13 @@
+# tugas : buatlah program dengan output sebagai berikut :
+
+nama = input("amrina rosyada")
+print("nama saya adalah ", nama)
+print(type(nama))
+
+umur = input("18")
+print("umur saya adalah", umur)
+print(type(umur))
+
+
+
+
